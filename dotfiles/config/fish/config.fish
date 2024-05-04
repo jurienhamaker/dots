6995fce5d30fcb5e455d9e4e918636f76b60c1b6
Jurien Hamaker <jurien@employes.nl>
2024-05-04 11:19:09 +0200

    refactor(fish): Don't show fastfetch when coming from ssh

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-n
+if test -z "$SSH_CLIENT"
+    n
+end
+
 zoxide init --cmd cd fish | source
 starship init fish | source
