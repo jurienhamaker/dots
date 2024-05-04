5bdbbad6d36ec907967644195ad97eea6f046a51
Jurien Hamaker <jurien@employes.nl>
2024-05-04 12:37:09 +0200

    feat: Add waynergy

--- dotfiles/config/hypr/scripts/is-not-home.sh
+++ dotfiles/config/hypr/scripts/is-not-home.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+while [ -z "$(ip addr show wlan0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)" ]; do
+	sleep 1
+done
+
 ip=$(ip -f inet -o addr show wlan0 | cut -d\  -f 7 | cut -d/ -f 1)
 
-if [ "$ip" == "192.168.1.40" ]; then
-	exit 1
+if [ "$ip" != "192.168.1.40" ]; then
+	$1
 fi
-
-exit 0
