7a43fcbcadda602278d3c692682b65e19a416f3b
Jurien Hamaker <whoami@jurien.dev>
2024-07-15 15:37:06 +0200

    feat(firefox): Add user.js

---
+++ dotfiles/mozilla/firefox/user.js
@@ -0,0 +1,13 @@
+// enable userchrome
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// default toolbox to right side
+user_pref("devtools.toolbox.host", "right");
+
+// trim urls
+user_pref("browser.urlbar.trimHttps", true);
+user_pref("browser.urlbar.trimURLs", true);
+
+// url bar tools
+user_pref("browser.urlbar.unitConversion.enabled", true);
+user_pref("browser.urlbar.suggest.calculator", true);
