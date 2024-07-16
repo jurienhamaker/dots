a443bea75570a074e7acc90d56a535aed51f2c20
Jurien Hamaker <whoami@jurien.dev>
2024-07-16 12:50:05 +0200

    feat(nvim): Fix ssh osc52 things

--- dotfiles/config/nvim/lua/config/options.lua
+++ dotfiles/config/nvim/lua/config/options.lua
@@ -11,3 +11,33 @@ vim.opt.autochdir = true
 vim.g.root_spec = { "cwd" }
 
 vim.o.shell = "fish"
+
+vim.opt.clipboard:append("unnamedplus")
+
+-- Fix "waiting for osc52 response from terminal" message
+-- https://github.com/neovim/neovim/issues/28611
+
+if vim.env.SSH_TTY ~= nil then
+  -- Set up clipboard for ssh
+
+  local function my_paste(_)
+    return function(_)
+      local content = vim.fn.getreg('"')
+      return vim.split(content, "\n")
+    end
+  end
+
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      -- No OSC52 paste action since wezterm doesn't support it
+      -- Should still paste from nvim
+      ["+"] = my_paste("+"),
+      ["*"] = my_paste("*"),
+    },
+  }
+end
