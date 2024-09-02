e516fd369caec62422df6759ce0ace5ff2ce7de6
Jurien Hamaker <whoami@jurien.dev>
2024-09-02 16:19:28 +0200

    fix(fish): Only run neofetch on interative shells

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,11 +1,10 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
 if test -e ~/.environment_variables
     export (cat ~/.environment_variables |xargs -L 1)
 end
 
-n
+if status is-interactive
+    n
+end
+
 zoxide init --cmd cd fish | source
 oh-my-posh init fish --config ~/.config/oh-my-posh/config.json | source
