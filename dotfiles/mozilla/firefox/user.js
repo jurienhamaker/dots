7c27b24995a17f9097946f739e3507e2550c722d
Jurien Hamaker <whoami@jurien.dev>
2024-09-14 18:51:38 +0200

    refactor(mozilla): Update user.js

--- dotfiles/mozilla/firefox/user.js
+++ dotfiles/mozilla/firefox/user.js
@@ -15,3 +15,16 @@ user_pref("browser.urlbar.suggest.calculator", true);
 // disable sharing indicator
 user_pref("privacy.webrtc.legacyGlobalIndicator", false);
 user_pref("privacy.webrtc.hideGlobalIndicator", true);
+
+// smooth scroll
+user_pref("general.smoothScroll.lines.durationMaxMS", 400);
+user_pref("general.smoothScroll.lines.durationMinMS", 200);
+user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 400);
+user_pref("general.smoothScroll.mouseWheel.durationMinMS", 400);
+user_pref("general.smoothScroll.other.durationMaxMS", 400);
+user_pref("general.smoothScroll.other.durationMinMS", 200);
+user_pref("general.smoothScroll.pages.durationMaxMS", 400);
+user_pref("general.smoothScroll.pages.durationMinMS", 200);
+user_pref("mousewheel.acceleration.start", 0);
+user_pref("mousewheel.min_line_scroll_amount", 50);
+user_pref("toolkit.scrollbox.smoothScroll", false);
