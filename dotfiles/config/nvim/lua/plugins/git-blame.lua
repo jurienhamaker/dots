4496fdbf92a58d65f42916d4980af01f3fe2806a
Jurien Hamaker <whoami@jurien.dev>
2024-09-20 16:05:17 +0200

    feat(nvim/git-blame): Update git blame format

--- dotfiles/config/nvim/lua/plugins/git-blame.lua
+++ dotfiles/config/nvim/lua/plugins/git-blame.lua
@@ -3,7 +3,8 @@ return {
 	event = "VeryLazy",
 	opts = {
 		enabled = true, -- if you want to enable the plugin
-		message_template = "<author> • <summary>", -- template for the blame message, check the Message template section for more options
-		date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+		message_template = "<author> • <date> • <summary>", -- template for the blame message, check the Message template section for more options
+		-- date_format = "%Y-%m-%d %H:%M:%S", -- template for the date, check Date format section for more options
+		date_format = "%Y-%m-%d", -- template for the date, check Date format section for more options
 	},
 }
