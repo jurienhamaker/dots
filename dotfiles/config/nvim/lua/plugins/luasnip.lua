6ee8f1c865129c051e1a1bf2ee9715a9fba9298e
Jurien Hamaker <whoami@jurien.dev>
2024-10-22 17:36:17 +0200

    fix(neovim/luasnip): Add ability to load local project snippets

--- dotfiles/config/nvim/lua/plugins/luasnip.lua
+++ dotfiles/config/nvim/lua/plugins/luasnip.lua
@@ -1,11 +1,24 @@
 return {
-  --  {
-  --    "L3MON4D3/LuaSnip",
-  --    opts = function()
-  --      require("luasnip.loaders.from_vscode").load_standalone({
-  --        paths = vim.fn.getcwd() .. "/.vscode/ngxs.code-snippets",
-  --        lazy = true,
-  --      })
-  --    end,
-  --  },
+	-- {
+	-- 	"L3MON4D3/LuaSnip",
+	-- 	config = function()
+	-- 		require("luasnip.loaders.from_vscode").lazy_load({
+	-- 			paths = vim.fn.getcwd(),
+	-- 		})
+	-- 	end,
+	-- },
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			config = function()
+				require("luasnip.loaders.from_vscode").lazy_load()
+				require("luasnip.loaders.from_vscode").lazy_load({ paths = vim.fn.getcwd() })
+			end,
+		},
+	},
+
+	{
+		"benfowler/telescope-luasnip.nvim",
+	},
 }
