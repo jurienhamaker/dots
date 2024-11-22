29ddcd8ff0d7527cb05c5c4a8c02b3a91e2bdc62
Jurien Hamaker <whoami@jurien.dev>
2024-11-22 11:31:06 +0100

    chore: reuse avante

--- dotfiles/config/nvim/lua/plugins/avante.lua
+++ dotfiles/config/nvim/lua/plugins/avante.lua
@@ -1,31 +1,44 @@
 return {
 	{
 		"yetone/avante.nvim",
-		enabled = false,
+		enabled = true,
 		event = "VeryLazy",
-		build = "make",
+		lazy = false,
+		version = false, -- set this if you want to always pull the latest change
 		opts = {
-			provider = "gemini",
+			-- provider = "gemini",
 			-- behaviour = {
 			-- 	auto_apply_diff_after_generation = true,
 			-- },
-			hints = {
-				enabled = false,
-			},
 			-- add any opts here
 		},
 		dependencies = {
-			"nvim-tree/nvim-web-devicons",
+			"nvim-treesitter/nvim-treesitter",
 			"stevearc/dressing.nvim",
 			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
 			{
-				"grapp-dev/nui-components.nvim",
-				dependencies = {
-					"MunifTanjim/nui.nvim",
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
 				},
 			},
-			--- The below is optional, make sure to setup it properly if you have lazy=true
 			{
+				-- Make sure to set this up properly if you have lazy=true
 				"MeanderingProgrammer/render-markdown.nvim",
 				opts = {
 					file_types = { "markdown", "Avante" },
