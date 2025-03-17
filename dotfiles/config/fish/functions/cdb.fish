14165b66e134e3ff04a67fe810cd621b651784c7
Jurien Hamaker <whoami@jurien.dev>
2025-03-17 11:10:54 +0100

    fix(fish/cdb): Set correct postgres prefix

--- dotfiles/config/fish/functions/cdb.fish
+++ dotfiles/config/fish/functions/cdb.fish
@@ -1,10 +1,10 @@
 function cdb
     if test "$argv[1]" = ""
-        set cdb_hostname "postgresql-development.employes.dev"
+        set cdb_hostname "employes-pg-dev.employes.dev"
     else
-        set cdb_hostname "postgresql-$argv[1].employes.dev"
+        set cdb_hostname "employes-pg-$argv[1].employes.dev"
     end
 
     echo "Connecting to \"$cdb_hostname\""
-    cloudflared access tcp --hostname $cdb_hostname --url localhost:54320;
+    cloudflared access tcp --hostname $cdb_hostname --url localhost:54320
 end
