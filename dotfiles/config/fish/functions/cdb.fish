accae180791b9932019b661888ebbdabdb46af25
Jurien Hamaker <whoami@jurien.dev>
2025-06-13 16:33:24 +0200

    feat(fish/cdb): Add ability to customize port

--- dotfiles/config/fish/functions/cdb.fish
+++ dotfiles/config/fish/functions/cdb.fish
@@ -5,6 +5,12 @@ function cdb
         set cdb_hostname "employes-pg-$argv[1].employes.dev"
     end
 
+    if test "$argv[2]" = ""
+        set cdb_port 5432
+    else
+        set cdb_port $argv[2]
+    end
+
     echo "Connecting to \"$cdb_hostname\""
-    cloudflared access tcp --hostname $cdb_hostname --url localhost:54320
+    cloudflared access tcp --hostname $cdb_hostname --url localhost:$cdb_port
 end
