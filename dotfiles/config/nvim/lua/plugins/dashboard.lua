e4fab8e5c2ef8eee5fb65761ae9036331aa40413
Jurien Hamaker <whoami@jurien.dev>
2026-01-24 11:50:42 +0100

    fix(nvim): Better way to change dashboard header

--- dotfiles/config/nvim/lua/plugins/dashboard.lua
+++ dotfiles/config/nvim/lua/plugins/dashboard.lua
@@ -1,50 +1,14 @@
 return {
 	{
 		"folke/snacks.nvim",
-		opts = function()
+		opts = function(_, opts)
 			local ascii = require("ascii")
 
 			local logo = ascii.art.text.neovim.sharp
 			local joined = table.concat(logo, "\n")
 
-			return {
-				dashboard = {
-					preset = {
-						header = joined,
-						keys = {
-							{
-								icon = " ",
-								key = "f",
-								desc = "Find File",
-								action = ":lua Snacks.dashboard.pick('files')",
-							},
-							{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-							{
-								icon = " ",
-								key = "g",
-								desc = "Find Text",
-								action = ":lua Snacks.dashboard.pick('live_grep')",
-							},
-							{
-								icon = " ",
-								key = "r",
-								desc = "Recent Files",
-								action = ":lua Snacks.dashboard.pick('oldfiles')",
-							},
-							{
-								icon = " ",
-								key = "c",
-								desc = "Config",
-								action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-							},
-							{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
-							{ icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
-							{ icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
-							{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
-						},
-					},
-				},
-			}
+			opts.dashboard.preset.header = joined
+			return opts
 		end,
 	},
 	{
